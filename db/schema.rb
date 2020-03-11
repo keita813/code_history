a485cb89f868abb3319c7532ae8cc9c8df1f45d3
keita813 <k1t37.smile@gmail.com>
2020-03-11 07:11:17 +0000

    [Finish]ログイン機能

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_03_10_093801) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "introduction"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
