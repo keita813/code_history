44289aacfecb06121f465e21705d405d9af7a6ac
keita813 <k1t37.smile@gmail.com>
2020-03-11 17:06:00 +0000

    [Start]新規投稿機能

---
+++ db/migrate/20200311165825_create_codes.rb
@@ -0,0 +1,12 @@
+class CreateCodes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :codes do |t|
+      t.integer :user_id
+      t.text :body
+      t.string :title
+      t.string :code_image
+
+      t.timestamps
+    end
+  end
+end
