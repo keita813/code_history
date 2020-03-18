dd847b6d0926681ce932143a90ac1d899e9cfa1e
keita813 <k1t37.smile@gmail.com>
2020-03-18 08:48:51 +0000

    [Finish]ユーザーページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :codes, dependent: :destroy
+
+  attachment :profiel_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'home#index'
   get 'home/about'
   devise_for :users
   resources :codes
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
