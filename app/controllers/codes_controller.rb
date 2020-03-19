9aee8a4c2d0b54208ec542f6a5b0a5158fce8f06
keita813 <k1t37.smile@gmail.com>
2020-03-19 07:52:08 +0000

    [Finish]投稿編集

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -21,18 +21,29 @@ class CodesController < ApplicationController
 	def show
 		@code = Code.find(params[:id])
 	end
+
 	def edit
 		@code = Code.find(params[:id])
-		
+		if @code.user != current_user
+			redirect_to code_path(@code)
+		end
 	end
+
 	def update
-		
+		@code = Code.find(params[:id])
+		if @code.update(code_params)
+			redirect_to code_path(@code)
+		else
+			render "edit"
+		end
+
 	end
 	def destroy
 		code = Code.find(params[:id])
 		code.destroy
 		redirect_to codes_path
 	end
+
 	private
 	def code_params
 		params.require(:code).permit(:title,:body)
