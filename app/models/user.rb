a3dcf3b5f5e6787599198bd68d5059a3111e2777
keita813 <k1t37.smile@gmail.com>
2020-03-22 17:32:32 +0000

    [Start]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   has_many :codes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
 
   attachment :profile_image
 end
+ has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
