3c4186840f960ac72b65d448f702fd1a401ed15b
keita813 <k1t37.smile@gmail.com>
2020-05-08 12:36:50 +0000

    [Update]非同期のいいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,15 +5,11 @@ class FavoritesController < ApplicationController
 		@code = Code.find(params[:code_id])
 		favorite = current_user.favorites.new(code_id: @code.id)
 		favorite.save
-		# 前の画面に戻る
-		# redirect_to request.referer
 	end
 
 	def destroy
 		@code = Code.find(params[:code_id])
 		favorite = current_user.favorites.find_by(code_id: @code.id)
 		favorite.destroy
-		# 前の画面に戻る
-		# redirect_to request.referer
 	end
 end
